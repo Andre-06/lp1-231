95b724658a188706a627f4806161636bb0371875
Andre Araujo <andre.araujosilva2006@gmail.com>
2023-03-17 02:00:48 -0300

    exercicio 04

--- src/semana04/exercicios/Ex04.java
+++ src/semana04/exercicios/Ex04.java
@@ -3,10 +3,39 @@ import java.util.Scanner;
 
 public class Ex04 {
     public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insira o código: ");
+        String cod = scan.nextLine();
+        
+        /*
+         * Exemplos válidos:
 
-        Scanner sc = new Scanner(System.in);
+BR0001X
+BR1236X
+BR9999X
+Exemplos inválidos:
 
-        System.out.println("");
+br0001X
+BR126X
+BR99999X
+BR9999Y
+         */
+
+        int nums;
+        try{
+            nums = Integer.parseInt(cod.substring(2, 6));
+        } catch (NumberFormatException exception){
+            System.out.println("Código Inválido");
+            return;
+        }
+        
+        if (cod.startsWith("BR") && cod.endsWith("X") && nums >= 1 && nums <= 9999 && cod.length() == 7) {
+            System.out.println("Codigo Válido");
+        } else {
+            System.out.println("Código Inválido");
+        }
+
 
         
     }
